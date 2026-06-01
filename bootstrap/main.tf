f697785bc289de371caa6ca0132b733e355631f6
Omer Edry <omere335@gmail.com>
2026-06-01 22:22:28 +0300

    prevent_destroy the s3 bucket

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "aws-load-balancer-terraform-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
