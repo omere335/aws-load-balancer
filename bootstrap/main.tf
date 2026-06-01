796e60a02b9c6ade85fd5ac3c2350656db1dbac9
Omer Edry <omere335@gmail.com>
2026-06-01 22:22:07 +0300

    remove dynamo_table
    since i use use_lock theres no need for it

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -25,14 +25,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
